51795dda2b12cced202d4dff5e207432e521c9aa
MarcelaMelendez <melendez.marcela@gmail.com>
2022-01-18 16:02:23 -0300

    se creó register.html y registerStyle.css

--- app.js
+++ app.js
@@ -10,4 +10,5 @@ const publicPath =path.resolve(__dirname,"./public")
 
 app.use (express.static(publicPath))
 
-app.get('/productCart', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productCart.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productCart.html')));
