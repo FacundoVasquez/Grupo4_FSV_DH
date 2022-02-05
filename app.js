be71d2597291b0517cb28bbb63c42600b6c569d3
Natalia Sandoval <sandoval_natalia@ymail.com>
2022-02-05 16:46:41 -0300

    es estructuraron archivos y cambios css login

--- app.js
+++ app.js
@@ -10,16 +10,13 @@ const publicPath =path.resolve(__dirname,"./public")
 
 app.use (express.static(publicPath))
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "src/views/index.html")));
 
-app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
-app.get('/productCart', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productCart.html')));
-app.get('/productCart', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productCart.html')));
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, "src/views/register.html")));
 
-app.get('/productDetail', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productDetail.html')));
+app.get('/productCart', (req, res) => res.sendFile(path.resolve(__dirname, "src/views/productCart.html")));
 
-app.get ("/login", function (req, res) {res.sendFile (path.resolve (__dirname, "./views/login.html"))});
+app.get('/productDetail', (req, res) => res.sendFile(path.resolve(__dirname, "src/views/productDetail.html")));
 
-app.get ("/login2", function (req, res) {res.sendFile (path.resolve (__dirname, "./views/login2.html"))});
-
+app.get ("/login", function (req, res) {res.sendFile (path.resolve (__dirname, "src/views/login.html"))});
 
