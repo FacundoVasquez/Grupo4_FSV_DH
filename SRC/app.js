30319f119aaacd7ff92c952b367c51f1375e517a
Natalia Sandoval <sandoval_natalia@ymail.com>
2022-02-08 15:24:46 -0300

    se crea el footer.ejs y su archivo Css. Se linkea en Index, Login y Register

--- SRC/app.js
+++ SRC/app.js
@@ -4,9 +4,11 @@ const app = express();
 
 const path =require("path");
 
-const publicPath = path.resolve(__dirname,"/public");
+const publicPath = path.resolve(__dirname,"../public");
 
 app.use (express.static(publicPath));
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.set("view engine", "ejs"); 
 
