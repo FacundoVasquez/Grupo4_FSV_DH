689bd6fe82e7f7e2a514eb8dfeddabe1b3533f40
Natalia Sandoval <sandoval_natalia@ymail.com>
2022-02-11 22:36:13 -0300

    se crea error 404, cambios de estilo register

--- SRC/app.js
+++ SRC/app.js
@@ -7,14 +7,14 @@ const path =require("path");
 const publicPath = path.resolve(__dirname,"../public");
 
 app.use (express.static(publicPath));
+app.use('/', mainRoutes);
+app.use ((req, res,next) => { res.status(404).render ("not-found")});
+
 
 app.set('views', path.join(__dirname, 'views'));
-
 app.set("view engine", "ejs"); 
 
 const mainRoutes = require("./routes/mainRoutes");
-
-app.use('/', mainRoutes);
 
 app.listen(3000, () => console.log ("Servidor Corriendo"));
 
