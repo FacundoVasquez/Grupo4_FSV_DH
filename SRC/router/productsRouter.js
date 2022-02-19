7bda50c94ffbe67093f3074c52e367bccc2f5e7a
fdrago95 <drago.federico95@gmail.com>
2022-02-19 10:21:51 -0300

    Update productsRouter.js

--- SRC/router/productsRouter.js
+++ SRC/router/productsRouter.js
@@ -12,7 +12,6 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         cb(null, file.filename + '_' + Date.now () + path.extname (file.originalname))},   
 });
-
 const upload=multer({storage:storage});
 
 //Rutas
