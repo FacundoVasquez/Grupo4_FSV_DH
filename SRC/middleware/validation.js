959e5c8efe80d0d3b8af8a183ae042d246c30851
Facundo Vasquez <facu.vasquez@gmail.com>
2022-03-20 20:25:34 -0300

    Correciones en validation

--- SRC/middleware/validation.js
+++ SRC/middleware/validation.js
@@ -5,7 +5,7 @@ const validation = [
 check("user_name").notEmpty().withMessage("El campo nombre y apellido no puede estar vacío"),
 check("email").notEmpty().withMessage("El campo E-mail no puede estar vacío").bail().isEmail().withMessage("E-mail: debe ser un e-mail"),
 check("password").notEmpty().withMessage("El campo contraseña no puede estar vacío").bail().isLength({min:6}).withMessage("La contraseña debe contener 6 caracteres mínimo"),
-check("password1").notEmpty().withMessage("El campo cnfirmación de contraseña no puede estar vacío").bail().isLength({min:6}).withMessage("La contraseña debe contener 6 caracteres mínimo"),
+check("password1").notEmpty().withMessage("El campo confirmación de contraseña no puede estar vacío").bail().isLength({min:6}).withMessage("La contraseña debe contener 6 caracteres mínimo"),
 check("password").equals("password1").withMessage("Las contraseñas no coinciden"),
 
 ]
